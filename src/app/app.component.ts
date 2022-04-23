8c63f591e7e9bbc5674f957b11240b4c0429fc44
heshanu <heshanu97@gmail.com>
2022-04-23 08:35:56 -0700

    added animation trigger state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('clickedState', [
+      state(
+        'default',
+        style({
+          backgroundColor: 'green',
+          'font-size': '20px',
+          height: '100px',
+          width: '100px',
+        })
+      ),
+      state(
+        'clicked',
+        style({
+          backgroundColor: 'red',
+          'font-size': '20px',
+          height: '100px',
+          width: '100px',
+        })
+      ),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'angularAnimation';
+
+  clickInfo: string = 'default';
+  onClickSimple() {
+    this.clickInfo = 'clicked';
+    setTimeout(()=>{
+      this.clickInfo='default';
+    },3000)
+  }
 }
